45945e7786afc1a7b505fe660f719fa7cba59861
Yaroslava <yaroslava.moskalova@gmail.com>
2025-10-20 13:34:22 +0200

    added router configuration

--- frontend/src/components/ExerciseCard/ExerciseCard.tsx
+++ frontend/src/components/ExerciseCard/ExerciseCard.tsx
@@ -11,7 +11,9 @@ export default function ExerciseCard(props: Readonly<ExerciseCardProps>) {
     const [reps, setReps] = useState(props.exercise.reps);
 
     return(
-        <>
-        </>
+        <div className="exercise-card">
+            <h3>ID: {props.exercise.id}</h3>
+            <h2>{name}: {sets}x{reps}</h2>
+        </div>
     )
 }
