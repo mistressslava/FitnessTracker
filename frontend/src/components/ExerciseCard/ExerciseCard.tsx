43b111a7c7004f868410ac15d77281c3b95fe05e
Yaroslava <yaroslava.moskalova@gmail.com>
2025-10-20 13:19:15 +0200

    added dependencies axios and created some components for exercises

---
+++ frontend/src/components/ExerciseCard/ExerciseCard.tsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+import type {Exercise} from "../../types/Exercise.ts";
+
+type ExerciseCardProps = {
+    exercise: Exercise,
+}
+
+export default function ExerciseCard(props: Readonly<ExerciseCardProps>) {
+    const [name, setName] = useState(props.exercise.name);
+    const [sets, setSets] = useState(props.exercise.sets);
+    const [reps, setReps] = useState(props.exercise.reps);
+
+    return(
+        <>
+        </>
+    )
+}
