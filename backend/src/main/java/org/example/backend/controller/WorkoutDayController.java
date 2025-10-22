d1f8dc10c7280dfc44965fb35d3b8891d9db37b5
Yaroslava <yaroslava.moskalova@gmail.com>
2025-10-22 11:57:43 +0200

    added controller, service and repo for workoutDay

--- backend/src/main/java/org/example/backend/controller/WorkoutDayController.java
+++ backend/src/main/java/org/example/backend/controller/WorkoutDayController.java
@@ -1,4 +1,30 @@
 package org.example.backend.controller;
 
+import org.example.backend.dto.WorkoutDayDto;
+import org.example.backend.model.WorkoutDay;
+import org.example.backend.service.WorkoutDayService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/workout-day")
 public class WorkoutDayController {
+
+    private final WorkoutDayService workoutDayService;
+
+
+    public WorkoutDayController(WorkoutDayService workoutDayService) {
+        this.workoutDayService = workoutDayService;
+    }
+
+    @GetMapping
+    public List<WorkoutDay> getAllWorkoutDays() {
+        return workoutDayService.getAllWorkoutDays();
+    }
+
+    @PostMapping
+    public WorkoutDay addWorkoutDay(@RequestBody WorkoutDayDto workoutDayDto) {
+        return workoutDayService.addWorkoutDay(workoutDayDto);
+    }
 }
