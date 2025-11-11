29a52053490c8c4b262bbe25befc46c738855fb1
Yaroslava <yaroslava.moskalova@gmail.com>
2025-11-12 00:35:15 +0100

    changed styling on home page

--- frontend/src/components/MainPage/MainPage.tsx
+++ frontend/src/components/MainPage/MainPage.tsx
@@ -1,8 +1,11 @@
+import {FeaturesSection} from "@/components/MainPage/FeatureSection.tsx";
+import {CTASection} from "@/components/MainPage/CTASection.tsx";
+
 export default function MainPage() {
 
     return (
         <>
-            <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20">
+            <div className="relative z-10 max-w-7xl px-4 sm:px-6 lg:px-8 pt-20">
                 <div className="text-center space-y-6">
                     <h1 className="text-5xl md:text-7xl font-bold text-foreground tracking-tight text-balance">
                         FORGE YOUR <span className="text-primary">STRENGTH</span>
@@ -16,13 +19,23 @@ export default function MainPage() {
                 <p className="text-xl text-foreground font-semibold mb-4">💪 About IronForge</p>
                 <p className="text-muted-foreground text-xl leading-relaxed mb-4">
                     IronForge is a smart fitness platform that helps you design and follow personalized workout plans —
-                    powered by AI technology. Whether you're a beginner or an experienced athlete, our system adapts to your
+                    powered by AI technology. Whether you're a beginner or an experienced athlete, our system adapts to
+                    your
                     goals, fitness level, and schedule. You can create your own workout plans or let our AI generate
-                    structured, science-based routines to help you build strength, improve endurance, and stay consistent.
+                    structured, science-based routines to help you build strength, improve endurance, and stay
+                    consistent.
                 </p>
                 <p className="text-primary font-semibold text-lg">
                     Train smarter, not harder — every rep, every day, built just for you.
                 </p>
+            </div>
+
+            <div
+                className="relative left-1/2 right-1/2 -ml-[50vw] -mr-[50vw] w-screen max-w-none overflow-x-hidden
+                [&>*]:mx-0 [&>*]:px-0 [&>*]:mt-0 [&>*]:mb-0"
+            >
+                <FeaturesSection/>
+                <CTASection/>
             </div>
         </>
     )
